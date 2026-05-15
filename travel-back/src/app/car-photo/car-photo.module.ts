04a0238661d3e5cbe79664619d1e7b2337b666a0
Maxim Sergeevich <nayzeok@gmail.com>
2026-05-15 14:52:37 +0700

    fix: add AuthModule to CarPhotoModule

--- travel-back/src/app/car-photo/car-photo.module.ts
+++ travel-back/src/app/car-photo/car-photo.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { DbModule } from '~/db/db.module'
+import { AuthModule } from '~/app/auth/auth.module'
 import { CarPhotoController } from './car-photo.controller'
 import { CarPhotoService } from './car-photo.service'
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, AuthModule],
   controllers: [CarPhotoController],
   providers: [CarPhotoService],
   exports: [CarPhotoService],
